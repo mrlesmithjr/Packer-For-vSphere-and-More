a1e9449ed80b4fae495061f6b92bfdf05ccc5ad5
Larry Smith Jr <mrlesmithjr@gmail.com>
2018-09-30 16:06:12 -0400

    Added VMware Tools install for Windows 2016

--- scripts/base.ps1
+++ scripts/base.ps1
@@ -12,3 +12,6 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; iex (
 # Enable Remote Desktop
 (Get-WmiObject Win32_TerminalServiceSetting -Namespace root\cimv2\TerminalServices).SetAllowTsConnections(1, 1)
 (Get-WmiObject -Class "Win32_TSGeneralSetting" -Namespace root\cimv2\TerminalServices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
+
+# Install Chocolatey Packages
+choco install -y 7zip
